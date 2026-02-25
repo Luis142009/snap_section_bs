13e4afc5162934d2d7020aa42ffd91f47d3b2696
juanLS07 <juanjose789t@gmail.com>
2026-02-25 11:29:03 -0500

    Snap_Secion

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+
+export const Home = () => {
+  return (
+    <>
+    
+<div className="container text-center">
+    <h2>row 1💖</h2>
+  <div className="row">
+    <div className="col-6 col-md-4 col-lg border">
+      Column
+    </div>
+    <div className="col-6 col-md-4 col-lg border">
+      Column
+    </div>
+    <div className="col-6 col-md-4 col-lg border">
+      Column
+    </div>
+     <div className="col-6 col-md-4 col-lg border">
+      Column
+    </div>
+  </div>
+  <h2>row 2🦊</h2>
+   <div className="row">
+    <div className="col-6 border">
+      Column
+    </div>
+    <div className="col-6 border">
+      Column
+    </div>
+    <div className="col-6 border">
+      Column
+    </div>
+     <div className="col-6 border">
+      Column
+    </div>
+    
+  </div>
+</div>
+
+
+    </>
+  )
+}
